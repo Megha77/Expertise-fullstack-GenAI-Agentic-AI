ab2fd3b15ae6129dab385170ba451f0c92b80c4d
Megha77 <meghakurwade7@gmail.com>
2026-08-07 01:27:46 +0530

    Module code added

---
+++ Python/Module/test.py
@@ -0,0 +1,10 @@
+import myModule
+
+a=5 
+b=6
+
+print("Greeting:", myModule.greet())
+print("Addition:", myModule.add(a, b))
+print("Subtraction:", myModule.subtract(a, b))
+print("Multiplication:", myModule.multiply(a, b))
+print("Division:", myModule.divide(a, b))
